bf50608f31f49f663fa717749f024221706012dd
Nathan Taylor <nbtaylor@gmail.com>
2019-08-23 17:10:01 -0700

    login/logout flow

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
-from flask import flash, redirect, render_template
-from flask_login import login_user
+from flask import flash, redirect, render_template, url_for
+from flask_login import login_required, login_user, logout_user
 from . import auth_blueprint
 
 from app import User
@@ -15,7 +15,14 @@ def login():
             flash('Invalid username or password.', 'danger')
             return render_template('auth/login.html', form=form)
         login_user(user, form.remember_me.data)
-        return redirect(url_for('main.index'))
+        return redirect(url_for('main.home_page'))
     else:
         # GET
         return render_template('auth/login.html', form=form)
+
+@auth_blueprint.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You have been logged out.")
+    return redirect(url_for("main.home_page"))
