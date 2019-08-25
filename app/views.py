96cf0fff2b74ff298266e1ed513173f88d29f772
Nathan Taylor <nbtaylor@gmail.com>
2019-08-24 19:14:09 -0700

    main page: concept count -> term count

--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, render_template
 
-from app.models import Concept
+from app.models import Term
 
 main_blueprint = Blueprint('main', __name__, template_folder='templates')
 
 @main_blueprint.route('/')
 def home_page():
-    concept_count = Concept.query.count()
-    return render_template('home_page.html', concept_count = concept_count)
+    term_count = Term.query.count()
+    return render_template('home_page.html', term_count=term_count)
 
