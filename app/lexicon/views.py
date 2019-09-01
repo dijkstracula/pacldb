2b7bfaaab601e615a9e297ed014f611c961f124b
Nathan Taylor <nbtaylor@gmail.com>
2019-09-01 16:07:29 -0700

    lexicon: remove Concept join

--- app/lexicon/views.py
+++ app/lexicon/views.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for, abort
 
-from app.models import Concept, Domain, Gloss, Language, Morph, Term
+from app.models import Domain, Gloss, Language, Morph, Term
 
 from . import lexicon_blueprint
 
 @lexicon_blueprint.route('/<ortho>', methods=['GET', 'POST'])
 def ortho_page(ortho):
-    query = Term.query.join(Concept).join(Language).join(Gloss).join(Domain).join(Morph)
+    query = Term.query.join(Language).join(Gloss).join(Domain).join(Morph)
     query = query.filter(Term.orthography == ortho)
 
     result = query.first()
