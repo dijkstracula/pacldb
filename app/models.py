c61ac2674c7bf6907f09f9c37c888129767ad7d8
Nathan Taylor <nbtaylor@gmail.com>
2020-02-09 20:13:45 -0700

    markdown: allow h4 tags
    wip

--- app/models.py
+++ app/models.py
@@ -18,7 +18,7 @@ class StaticContent(db.Model):
     def on_changed_body(target, value, oldvalue, initiator):
         allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
                         'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
-                        'h1', 'h2', 'h3', 'p']
+                        'h1', 'h2', 'h3', 'h4', 'p', 'img']
         target.body_html = bleach.linkify(bleach.clean(
             markdown(value, output_format="html"),
             tags=allowed_tags, strip=True))
@@ -51,7 +51,7 @@ class Language(db.Model):
     def on_changed_concordance(target, value, oldvalue, initiator):
         allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
                         'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
-                        'h1', 'h2', 'h3', 'p']
+                        'h1', 'h2', 'h3', 'h4', 'p', 'img']
         target.body_html = bleach.linkify(bleach.clean(
             markdown(value, output_format="html"),
             tags=allowed_tags, strip=True))
